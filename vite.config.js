a3335b2b0e537fc3fd1f8fe72901562b07731e76
Julieth Florez <kslopez89@soy.sena.edu.co>
2025-06-13 14:53:07 -0500

    configuracion de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    middlewareMode: true,
+  },
+  configureServer: (server) => {
+    server.middlewares.use(history());
+  },
+});
+
